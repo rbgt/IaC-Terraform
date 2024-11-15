ae516b13d4ecb735a0ad1b8e64e888bf4f8a5706
rickantonais <rickantonais@outlook.com>
2024-11-15 17:01:13 +0100

    feat: replacing subscription id, tenant id and client id with github actions secrets

--- backend.tf
+++ backend.tf
@@ -7,8 +7,8 @@ terraform {
     # Touchy part to authenticate with Azure and deploy the resources
     use_azuread_auth = true
     use_oidc = true
-    subscription_id = "f2b69101-46bb-4437-b7e3-4db306ddcd12"
-    tenant_id = "d0eabb3d-a232-4374-ac61-2c4f5849a522"
-    client_id = "e7ee42bd-aa24-44d5-b519-cdc467ed3cfd"
+    subscription_id = ${{ secrets.TF_VAR_SUBSCRIPTION_ID}}
+    tenant_id = ${{ secrets.TF_VAR_TENANT_ID}}
+    client_id = ${{ secrets.TF_VAR_CLIENT_ID}}
   }
 }
--- provider.tf
+++ provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "f2b69101-46bb-4437-b7e3-4db306ddcd12"
+  subscription_id = ${{ secrets.TF_VAR_SUBSCRIPTION_ID}}
 }
