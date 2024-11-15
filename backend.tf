8d55b6e621e2a2ef2163cb37041e1aed49164cb5
rickantonais <rickantonais@outlook.com>
2024-11-15 17:52:35 +0100

    feat: Cleaned up template!

--- backend.tf
+++ backend.tf
@@ -7,8 +7,5 @@ terraform {
     # Touchy part to authenticate with Azure and deploy the resources
     use_azuread_auth = true
     use_oidc = true
-    # subscription_id = ${{ secrets.TF_VAR_SUBSCRIPTION_ID}}
-    # tenant_id = ${{ secrets.TF_VAR_TENANT_ID}}
-    # client_id = ${{ secrets.TF_VAR_CLIENT_ID}}
   }
 }
