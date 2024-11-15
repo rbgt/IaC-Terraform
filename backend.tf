1657611b879e6f7a6155d305b8573fd275ddff39
rickantonais <rickantonais@outlook.com>
2024-11-15 15:24:23 +0100

    feat: change azurerm backend to have authentication with service principal

--- backend.tf
+++ backend.tf
@@ -4,5 +4,7 @@ terraform {
     storage_account_name = "tfstateblobproject"
     container_name       = "tfstatecontainer"
     key                  = "projectstate.tfstate"
+    use_azuread_auth = true
+    use_oidc = true
   }
 }
